02a9dc3913a7ef631a862632d89ce21aa9d0d311
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-08-12 20:40:09 -0400

    add: ex em aula

---
+++ 12-08-2024/ex1.py
@@ -0,0 +1,8 @@
+a = int(input("Digite um número inteiro: "))
+
+if (a % 2) == 0:
+    print("Número par")
+if (a % 2) == 1:
+    print("Número ímpar")
+
+print("Fim do programa")
---
+++ 12-08-2024/ex2.py
@@ -0,0 +1,8 @@
+a = int (input("Digite um numero: "))
+
+impar = ((a%2) == 1)
+if impar: 
+    print("Impar")
+if not (impar): 
+    print("Par")
+print ("Fim do programa")

72a49c4ba1e6fb2c5b103f2bde165575feb32fc6
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-08-13 00:56:47 +0000

    feat: exercise done, but correction needed

---
+++ ex4.py
@@ -0,0 +1,9 @@
+a = int(input("Digite um numero: "))
+b = int(input("Digite um numero: "))
+
+if a == b:
+    print("Os dois numeros são iguais")
+elif a > b: 
+    print("O a é maior que b")
+elif b > a: 
+    print("O b é maior que a")

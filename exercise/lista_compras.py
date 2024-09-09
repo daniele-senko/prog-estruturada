7d2c98a78b161d87ac8f009edf8eb3f2ebfa7cd4
Daniele Senko <142120811+daniele-senko@users.noreply.github.com>
2024-09-09 13:31:43 -0400

    remove: remove 'milk' from list

--- exercise/lista_compras.py
+++ exercise/lista_compras.py
@@ -3,4 +3,7 @@ lista_compras = ["maça", "banana", "leite", "pão"]
 # adicionando item na lista
 lista_compras.append("ovos")
 
+# removendo item da lista
+lista_compras.remove("leite")
+
 print(lista_compras)
